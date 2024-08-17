085dcb8bb6e9f498ca9d2991c7a494620050dd97
harshv06 <harshvonmail@gmail.com>
2024-08-17 23:52:56 +0530

    Changes

--- Program.cs
+++ Program.cs
@@ -1,87 +1,27 @@
-﻿using System;
-
-int[] times = {800, 1200, 1600, 2000};
-int diff = 0;
-
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Current Medicine Schedule:");
-
-/* Format and display medicine times */
-foreach (int val in times)
+﻿try
 {
-    string time = val.ToString();
-    int len = time.Length;
-
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
-
-    Console.Write($"{time} ");
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
 }
 
-Console.WriteLine();
+Console.WriteLine("Exit program");
 
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
-
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+static void Process1()
 {
-    Console.WriteLine("Invalid GMT");
-}
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
-{
-    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++) 
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
-} 
-else 
-{
-    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-
-    /* Adjust the times by adding the difference, keeping the value within 24 hours */
-    for (int i = 0; i < times.Length; i++) 
-    {
-        times[i] = ((times[i] + diff)) % 2400;
-    }
+    WriteMessage();
 }
 
-Console.WriteLine("New Medicine Schedule:");
-
-/* Format and display medicine times */
-foreach (int val in times)
+static void WriteMessage()
 {
-    string time = val.ToString();
-    int len = time.Length;
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+    string name="Hars";
 
-    if (len >= 3)
-    {
-        time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-        time = time.Insert(0, "0:");
-    }
-    else
-    {
-        time = time.Insert(0, "0:0");
-    }
-
-    Console.Write($"{time} ");
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
 }
-
-Console.WriteLine();
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28ea12c6ff0dc922091363c2a3e752f84b6b79e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
